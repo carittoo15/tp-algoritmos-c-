d667510408d144dca3959ab5d63453b1e1e97097
carittoo15 <caro15rimini@gmail.com>
2025-10-20 23:09:03 -0300

    terminado

--- Figura.cs
+++ Figura.cs
@@ -67,4 +67,53 @@ namespace Figuras
             graphics.DrawEllipse(pen, x, y, radio, radio);
         }
     }
+    // Agrego una clase triangulo isosceles que se dibuja con tres puntos (dos iguales y uno distinto)
+    public class TrianguloIsosceles : Figura
+    {
+        private int baseTriangulo;
+        private int altura;
+
+        public TrianguloIsosceles(int baseTriangulo, int altura)
+        {
+            this.baseTriangulo = baseTriangulo;
+            this.altura = altura;
+        }
+
+        public override void Dibujar(Pen pen, Graphics graphics, int x, int y)
+        {
+            Point[] puntos = new Point[3]
+            {
+                new Point(x, y + altura),               // esquina inferior izquierda
+                new Point(x + baseTriangulo / 2, y),    // vértice superior
+                new Point(x + baseTriangulo, y + altura)// esquina inferior derecha
+            };
+            graphics.DrawPolygon(pen, puntos);
+        }
+    }
+
+    // Agrego una clase q se llame triangulo equilatero con lados iguales y uso Math.Sqrt(3)/2*l para calcular la altura
+    public class TrianguloEquilatero : Figura
+    {
+        private int lado;
+
+        public TrianguloEquilatero(int lado)
+        {
+            this.lado = lado;
+        }
+
+        public override void Dibujar(Pen pen, Graphics graphics, int x, int y)
+        {
+            // Calculo la altura del triangulp equilatero
+            int altura = (int)(Math.Sqrt(3) / 2 * lado);
+
+            Point[] puntos = new Point[3]
+            {
+                new Point(x, y + altura),           // esquina inferior izquierda
+                new Point(x + lado / 2, y),         // vértice superior
+                new Point(x + lado, y + altura)     // esquina inferior derecha
+            };
+            graphics.DrawPolygon(pen, puntos);
+        }
+    }
+
 }
